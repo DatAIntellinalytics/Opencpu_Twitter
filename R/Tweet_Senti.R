d24513d9bd08e3c4b001981601e87950e89967b6
Arasaraja <arasa_raja@yahoo.com>
2017-07-19 19:26:46 +0530

    Add files via upload

--- R/Tweet_Senti.R
+++ R/Tweet_Senti.R
@@ -53,6 +53,9 @@ tweet_senti <- function (df_tweets) {
   library(caret)
   library(glmnet)
   library(ggrepel)
+  library(plyr)
+  library(dplyr)
+  library(ggplot2)
   #library(purrrlyr, lib.loc = "library/")
   #library(purrrlyr)
 
@@ -109,44 +112,62 @@ preds_tweets <- predict(glmnet_classifier, dtm_tweets_tfidf, type = 'response')[
 # adding rates to initial dataset
 df_tweets$sentiment <- preds_tweets
 
-#return(df_tweets)
+#total score calculation: positive / negative / neutral
+df_tweets$created <- as.Date(df_tweets$created , format = "%m/%d/%Y")
+df_tweets <- mutate(df_tweets, score=ifelse(df_tweets$sentiment > 0.65, 'positive', ifelse(df_tweets$sentiment < 0.35, 'negative', 'neutral')))
+by.tweet <- group_by(df_tweets, score, created)
+by.tweet <- summarise(by.tweet, number=n())
 
-#write.csv(df_tweets,"tweet_sentiment.csv",row.names = FALSE)
+#chart
+ggplot(by.tweet, aes(created, number)) + geom_line(aes(group=score, color=score), size=2) +
+  geom_point(aes(group=score, color=score), size=4) +
+  theme(text = element_text(size=18), axis.text.x = element_text(angle=90, vjust=1)) +
+ #stat_summary(fun.y = 'sum', fun.ymin='sum', fun.ymax='sum', colour = 'yellow', size=2, geom = 'line') +
+  ggtitle('Humira')
 
-# color palette
- cols <- c("#ce472e", "#f05336", "#ffd73e", "#eec73a", "#4ab04a")
 
- set.seed(932)
- samp_ind <- sample(c(1:nrow(df_tweets)), nrow(df_tweets) * 0.1) # 10% for labeling
+# #return(df_tweets)
 #
-# # plotting
-#plt <-
-  ggplot(df_tweets, aes(x = created, y = sentiment, color = sentiment)) +
-   theme_minimal() +
-   scale_color_gradientn(colors = cols, limits = c(0, 1),
-                         breaks = seq(0, 1, by = 1/4),
-                         labels = c("0", round(1/4*1, 1), round(1/4*2, 1), round(1/4*3, 1), round(1/4*4, 1)),
-                         guide = guide_colourbar(ticks = T, nbin = 50, barheight = .5, label = T, barwidth = 10)) +
-   geom_point(aes(color = sentiment), alpha = 0.8) +
-   geom_hline(yintercept = 0.65, color = "#4ab04a", size = 1.5, alpha = 0.6, linetype = "longdash") +
-   geom_hline(yintercept = 0.35, color = "#f05336", size = 1.5, alpha = 0.6, linetype = "longdash") +
-   geom_smooth(size = 1.2, alpha = 0.2) +
-   geom_label_repel(data = df_tweets[samp_ind, ],
-                    aes(label = round(sentiment, 2)),
-                    fontface = 'bold',
-                    size = 2.5,
-                    max.iter = 100) +
-   theme(legend.position = 'bottom',
-         legend.direction = "horizontal",
-         panel.grid.major = element_blank(),
-         panel.grid.minor = element_blank(),
-         plot.title = element_text(size = 20, face = "bold", vjust = 2, color = 'black', lineheight = 0.8),
-         axis.title.x = element_text(size = 16),
-         axis.title.y = element_text(size = 16),
-         axis.text.y = element_text(size = 8, face = "bold", color = 'black'),
-         axis.text.x = element_text(size = 8, face = "bold", color = 'black')) +
-   ggtitle("Tweets Sentiment rate (probability of positiveness)")
+# #write.csv(df_tweets,"tweet_sentiment.csv",row.names = FALSE)
+#
+# # color palette
+#  cols <- c("#ce472e", "#f05336", "#ffd73e", "#eec73a", "#4ab04a")
+#
+#  set.seed(932)
+#  samp_ind <- sample(c(1:nrow(df_tweets)), nrow(df_tweets) * 0.1) # 10% for labeling
+# #
+# # # plotting
+# #plt <-
+#   ggplot(df_tweets, aes(x = created, y = sentiment, color = sentiment)) +
+#    theme_minimal() +
+#    scale_color_gradientn(colors = cols, limits = c(0, 1),
+#                          breaks = seq(0, 1, by = 1/4),
+#                          labels = c("0", round(1/4*1, 1), round(1/4*2, 1), round(1/4*3, 1), round(1/4*4, 1)),
+#                          guide = guide_colourbar(ticks = T, nbin = 50, barheight = .5, label = T, barwidth = 10)) +
+#    geom_point(aes(color = sentiment), alpha = 0.8) +
+#    geom_hline(yintercept = 0.65, color = "#4ab04a", size = 1.5, alpha = 0.6, linetype = "longdash") +
+#    geom_hline(yintercept = 0.35, color = "#f05336", size = 1.5, alpha = 0.6, linetype = "longdash") +
+#    geom_smooth(size = 1.2, alpha = 0.2) +
+#    geom_label_repel(data = df_tweets[samp_ind, ],
+#                     aes(label = round(sentiment, 2)),
+#                     fontface = 'bold',
+#                     size = 2.5,
+#                     max.iter = 100) +
+#    theme(legend.position = 'bottom',
+#          legend.direction = "horizontal",
+#          panel.grid.major = element_blank(),
+#          panel.grid.minor = element_blank(),
+#          plot.title = element_text(size = 20, face = "bold", vjust = 2, color = 'black', lineheight = 0.8),
+#          axis.title.x = element_text(size = 16),
+#          axis.title.y = element_text(size = 16),
+#          axis.text.y = element_text(size = 8, face = "bold", color = 'black'),
+#          axis.text.x = element_text(size = 8, face = "bold", color = 'black')) +
+#    ggtitle("Tweets Sentiment rate (probability of positiveness)")
 
 # #return(plt)
 
 }
+
+
+
+
