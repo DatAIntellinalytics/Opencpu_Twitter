cdcac07a815dd040cecff01941aabebd42c6200d
Arasaraja <arasa_raja@yahoo.com>
2017-07-19 18:18:38 +0530

    Update Tweet_Senti.R

--- R/Tweet_Senti.R
+++ R/Tweet_Senti.R
@@ -44,9 +44,6 @@
 
 
 tweet_senti <- function (df_tweets) {
- id <- as.integer(df_tweets$id)
-  text <- as.character(df_tweets$text)
-  created <- as.character(df_tweets$created)
   df_tweets <- data.frame(id,text,created)
 df_tweets$id <- as.integer(df_tweets$id)
  df_tweets$text <- as.character(df_tweets$text)
